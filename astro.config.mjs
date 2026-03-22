a05c77b1f9a7630996b79278e74b3857e6f16e7e
emilianoscheli <emilianocheli19@gmail.com>
2026-03-22 19:38:09 -0300

    first commit fifa world cup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; // <-- 1. Importamos el sitemap
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fifaworld-cup-2026.org', // <-- 2. OBLIGATORIO: Tu dominio en producción
+  integrations: [sitemap()],      // <-- 3. Agregamos el sitemap a las integraciones
+  vite: {
+    plugins: [tailwindcss()]      // (Tailwind sigue igual, adentro de vite)
+  }
+});
