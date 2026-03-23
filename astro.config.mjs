562ffc5e1741b2de956e838d5f15bb18af95e78d
emilianoscheli <emilianocheli19@gmail.com>
2026-03-22 21:00:15 -0300

    Agrego la barra al final para sitemapa

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap'; // <-- 1. Importamos el sitemap
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fifaworld-cup-2026.org', // <-- 2. OBLIGATORIO: Tu dominio en producción
+  site: 'https://fifaworld-cup-2026.org/', // <-- 2. OBLIGATORIO: Tu dominio en producción
   integrations: [sitemap()],      // <-- 3. Agregamos el sitemap a las integraciones
   vite: {
     plugins: [tailwindcss()]      // (Tailwind sigue igual, adentro de vite)
